0219519257776972d6277a5dd312214e1f19bce1
shivamvku <shivam.vku@gmail.com>
2022-10-20 18:09:32 +0530

    added search function

--- flaskfrontend/src/App.js
+++ flaskfrontend/src/App.js
@@ -1,10 +1,24 @@
+import { useState } from 'react'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/search';
 
-function App() {
+
+
+
+const App = () => {
+  const [word, setWord] = useState('');
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  }
+
+  
   return (
     <div className="App">
-     <Header title='Images Gallery 2'/>
+     <Header title='Images Gallery'/>
+     <Search word = {word} setWord={setWord} handleSubmit = {handleSearchSubmit}/>
     </div>
   );
 }
--- flaskfrontend/src/components/Header.js
+++ flaskfrontend/src/components/Header.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
 
-const Header = (props) => {
-    const {title} = props;
+const navbarStyle = {
+  backgroundColor: 'Lightblue'
+};
+const Header = ({title}) => {
     return(
-        <Navbar bg="light" variant="light">
-          <Navbar.Brand href="/">{props.title}</Navbar.Brand>         
+        <Navbar style={navbarStyle} variant="light">
+          <Container>
+          <Navbar.Brand href="/">{title}</Navbar.Brand>
+          </Container>
       </Navbar>
     )
 };
