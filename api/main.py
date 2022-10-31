b00d0ca54b3501c8c5cc8e64ff7cb7315aecfef2
shivamvku <shivam.vku@gmail.com>
2022-10-31 22:57:52 +0530

    enabled Debug

--- api/main.py
+++ api/main.py
@@ -1,3 +1,4 @@
+from distutils.debug import DEBUG
 from flask import Flask, request 
 import requests  
 import os 
@@ -7,16 +8,17 @@ load_dotenv(dotenv_path="./.env.local")
 
 UNSPLASH_URL = "https://api.unsplash.com/photos/random/"
 UNSPLASH_KEY= os.environ.get("UNSPLASH_KEY", "")
-
+DEBUG=bool(os.environ.get("DEBUG", True))
 if not UNSPLASH_KEY:
     raise EnvironmentError("Please create .env.local file and nister the UNSPLASH_KEY there")
     
-# UNSPLASH_KEY = "8lO7Sgx6euAg4LX18LzWl40GB-TnAu1cn8jxzSEg54g"
 
 
 
 
 app = Flask(__name__)
+
+app.config["DEBUG"] = DEBUG
 
 @app.route("/newimg")
 def newimg():
